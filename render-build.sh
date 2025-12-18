d018fd71ab9e049c0486d005b89df4e9dfe49672
OM Sharma <155946656+Cypher-redeye@users.noreply.github.com>
2025-12-18 23:46:51 +0530

    Fix NLTK typo and enable model training on build

--- render-build.sh
+++ render-build.sh
@@ -5,4 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 
 # Download NLTK data
-python -m nltk.downloader punkt wordnet omniw_1.4
+python -m nltk.downloader punkt wordnet omw-1.4
+python src/train.py
